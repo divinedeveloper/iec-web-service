2bd7e5e17e6389e366579ee0aa4c37cc1f88b172
Swapnil Sawant <sawant.swapnil1@gmail.com>
2017-05-20 04:40:56 +0530

    hacking mongodb instalation on ubuntu 15.04 and connecting application with mongod using django-rest-framework-mongoengine

--- iec/iec/settings/base.py
+++ iec/iec/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import mongoengine
 
 ########## PATH CONFIGURATION
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = os.environ['A_SECRET_KEY']
 # Read later from a non versioned file
 
-# SECRET_KEY = os.environ['A_SECRET_KEY']
+SECRET_KEY = 'fip!($r&3_@n5%*ldqcwggk++^cdto209l$pv_mnuzzsow0)@1'
 ########## END SECRET CONFIGURATION
 
 
@@ -54,6 +55,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_mongoengine',
 
     'iec',
 )
@@ -104,12 +106,12 @@ WSGI_APPLICATION = 'iec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 ########## END DATABASE CONFIGURATION
 
--- iec/iec/settings/production.py
+++ iec/iec/settings/production.py
@@ -7,11 +7,25 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+_MONGODB_USER = 'iecadmin'
+_MONGODB_PASSWD = 'iecpass'
+_MONGODB_HOST = '127.0.0.1'
+_MONGODB_PORT = '27017'
+_MONGODB_NAME = 'iec'
+_MONGODB_DATABASE_HOST = \
+    'mongodb://%s:%s@%s:%s/%s' \
+    % (_MONGODB_USER, _MONGODB_PASSWD, _MONGODB_HOST, _MONGODB_PORT, _MONGODB_NAME)
+
+# from pymongo import MongoClient
+try:
+	# client = MongoClient('mongodb://iecadmin:iecpass@127.0.0.1:27017/iec')
+	# db = client.test
+	# coll = db.dataset
+	# result = db.restaurants.insert_one({"borough": "Manhattan"})
+	# print result.inserted_id
+
+	mongoengine.connect(_MONGODB_NAME, host= _MONGODB_DATABASE_HOST)
+except Exception as e:
+    print '%s (%s)' % (e.message, type(e))
 
 ########## END DATABASE CONFIGURATION
